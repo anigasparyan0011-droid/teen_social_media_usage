ac910e78ab46551934f2eab8a665385503cf911f
anigasparyan0011-droid <ani.gasparyan0011@gmail.com>
2026-05-22 01:51:38 +0400

    Delete transform.py

--- transform.py
+++
@@ -1,8 +0,0 @@
-def transform_data(dfs):
-    """
-    No transformation is applied here.
-    The CSV is loaded as one raw table.
-    Normalization will be done later in SQLite using SQL.
-    """
-
-    return dfs
