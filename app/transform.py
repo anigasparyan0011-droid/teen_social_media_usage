85de7265233f1f8994c69f61d74ad84eac746146
anigasparyan0011-droid <ani.gasparyan0011@gmail.com>
2026-05-22 01:50:32 +0400

    Add files via upload

---
+++ app/transform.py
@@ -0,0 +1,8 @@
+def transform_data(dfs):
+    """
+    No transformation is applied here.
+    The CSV is loaded as one raw table.
+    Normalization will be done later in SQLite using SQL.
+    """
+
+    return dfs
